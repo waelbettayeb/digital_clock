193eb6d01ddbb92eeafc5e7c0216505c34429536
WaelMohamedKhelil <khelilwael@gmail.com>
2019-12-29 16:55:22 +0100

    create time seperator widget

---
+++ digital_clock/lib/time_separator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class TimeSeperator extends StatefulWidget {
+  const TimeSeperator(this.time);
+
+  final DateTime time;
+
+  @override
+  _TimeSeperator createState() => _TimeSeperator();
+}
+class _TimeSeperator extends State<TimeSeperator>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
